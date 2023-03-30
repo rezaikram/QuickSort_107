53993f2dbb011a671fadd7ded82d12bd6ddad0e9
rezaikram <muhammadikramriza@gmail.com>
2023-03-30 21:15:50 +0700

    Langkah algoritma 8

--- QuickSort_107/QuickSort_107.cpp
+++ QuickSort_107/QuickSort_107.cpp
@@ -66,4 +66,10 @@ void q_short(int low, int high)
 		//search for an element less than or equal to pivot
 		while ((arr[j] > pivot) && (j >= low))		//Langkah Algoritma No. 7
 		{
-			
+			j--;									//Langkah Algoritma No. 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+		// if the greater element is on the left of the element
+		
